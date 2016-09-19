2531724b2bdc30cad44faa011bd932fbcdbc986c
Lázaro Vieira <lazaro-e-nemo@hotmail.com>
2016-09-18 22:47:59 -0300

    CSS clean-up; updated database

--- homebrew/hbdb.js
+++ homebrew/hbdb.js
@@ -7,9 +7,9 @@ if (document.location.search.substring(1).length > 0) {
 }
 
 var lang = "";
-if (queries.lang!=undefined) {
+if (queries.nolang==undefined && queries.lang!=undefined) {
     lang = queries.lang;
-    window.location.replace("http://ryuzaki-mrl.github.io/3ds/homebrew/"+lang);
+    window.location.replace("http://ryuzaki-mrl.github.io/3ds/homebrew/"+lang+"?nolang=&"+document.location.search.substring(1));
 }
 
 $.getJSON("hbdb.json", function(json) {
@@ -17,14 +17,14 @@ $.getJSON("hbdb.json", function(json) {
         $('.list').append (
             '<li class="'+this.type+'">' +
             '<div id="'+$(json).index(this)+'">' +
-            '<span class="icon"><img class="i" src="/3ds/homebrew/icons/'+$(json).index(this)+'.png"/></span>' +
-            '<span class="title"><b>'+this.title+'</b></span>' +
-            '<span class="desc">'+this.desc+'</span>' +
-            '<span class="author">Author: '+this.author+'</span>' +
-            '<span class="tags">'+this.tags+'</span>' +
-            '<span class="release">'+this.date+'</span>' +
-            '<span class="cat">'+this.type+'</span>' +
-            '<span class="status">'+(this.status.length==0 ? 'unknown' : this.status)+'</span>' +
+            '<span class="icon"><img src="/3ds/homebrew/icons/'+$(json).index(this)+'.png"/></span>' +
+            '<span class="vdata title"><b>'+this.title+'</b></span>' +
+            '<span class="vdata desc">'+this.desc+'</span>' +
+            '<span class="vdata author">Author: '+this.author+'</span>' +
+            '<span class="hdata tags">'+this.tags+'</span>' +
+            '<span class="hdata release">'+this.date+'</span>' +
+            '<span class="hdata cat">'+this.type+'</span>' +
+            '<span class="hdata status">'+(this.status.length==0 ? 'unknown' : this.status)+'</span>' +
             '</div></li>'
         );
     });
@@ -36,47 +36,47 @@ $.getJSON("hbdb.json", function(json) {
         "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
     ];
 
-    $('.i').error(function() {
+    $('.icon img').error(function() {
         $(this).attr('src', '/3ds/homebrew/icons/noicon.png');
     });
 
-    $('.screenshot').error(function() {
+    $('.screenshot img').error(function() {
         $(this).attr('src', '/3ds/homebrew/screenshots/noscreen.png');
     });
 
     $('.screenshot').click(function() {
-        $('.hb_entry p, .hb_entry h1, .hb_entry table, .hb_entry span:not(#sshot)').fadeOut(120);
-        $('.screenshot').animate({width: $('.screenshot').css('width')=='120px' ? 400 : 120}, 360);
-        if ($('.screenshot').css('width')!='120px') $('.hb_entry *' + ($('.show').hasClass('less') ? ':not(.compat)' : '')).fadeIn(500);
+        $('.details p, .details h1, .details table, .details span:not(.screenshot)').fadeOut(120);
+        $('.screenshot img').animate({width: $('.screenshot img').css('width')=='120px' ? 400 : 120}, 360);
+        if ($('.screenshot img').css('width')!='120px') $('.details *' + ($('.show').hasClass('less') ? ':not(.compat)' : '')).fadeIn(500);
     });
 
     function showDetails() {
-        $('.hb_entry, .hb_entry *').fadeIn(500);
-        $('.screenshot').css('width', '120px');
-        $('.hb_entry .i').attr('src', '/3ds/homebrew/icons/'+id+'.png');
-        $('.hb_entry #title').html(json[id].title);
-        $('.hb_entry #title').css('fontSize', 36 - Math.floor(json[id].title.length / 2.5));
-        $('.hb_entry #author').html(json[id].author);
+        $('.details, .details *').fadeIn(500);
+        $('.screenshot img').css('width', '120px');
+        $('.details .icon img').attr('src', '/3ds/homebrew/icons/'+id+'.png');
+        $('.details #title').html(json[id].title);
+        $('.details #title').css('fontSize', 36 - Math.floor(json[id].title.length / 2.5));
+        $('.details #author').html(json[id].author);
         var year = json[id].date.substring(0,4);
         var month = monthstr[parseInt(json[id].date.substring(4,6))];
         var day = json[id].date.substring(6,8);
         var date = (json[id].date=="0") ? "Unreleased" : (json[id].date.length==0) ? "Unknown" : (month + " " + day + ", " + year);
-        $('.hb_entry #release').html(date);
-        $('.hb_entry #version').html(date!="Unreleased" ? (json[id].ver.length > 0 ? json[id].ver : "Unknown") : '-');
-        $('.hb_entry #site').html(json[id].site.length > 0 ? ('<a href="'+json[id].site+'" target="_blank">'+'<img src="btm_e.png"/>'+'</a>') : '-');
+        $('.details #release').html(date);
+        $('.details #version').html(date!="Unreleased" ? (json[id].ver.length > 0 ? json[id].ver : "Unknown") : '-');
+        $('.details #site').html(json[id].site.length > 0 ? ('<a href="'+json[id].site+'" target="_blank">'+'<img src="btm_e.png"/>'+'</a>') : '-');
         if (json[id].long.length > 256) {
-            $('.hb_entry #desc').html(json[id].long.substring(0,240) + '... ');
+            $('.details .desc').html(json[id].long.substring(0,240) + '... ');
             $('.show').addClass('more');
             $('.show').removeClass('less');
             $('.show').show();
         } else {
-            $('.hb_entry #desc').html(json[id].long.length > 0 ? json[id].long : json[id].desc);
+            $('.details .desc').html(json[id].long.length > 0 ? json[id].long : json[id].desc);
             $('.show').hide();
             $('.show').removeClass('more less');
         }
-        $('.hb_entry .screenshot').attr('src', '');
-        $('.hb_entry .screenshot').attr('src', '/3ds/homebrew/screenshots/'+id+'.png');
-        $('.hb_entry table td').each(function(index) {
+        $('.screenshot img').attr('src', '');
+        $('.screenshot img').attr('src', '/3ds/homebrew/screenshots/'+id+'.png');
+        $('.details table td').each(function(index) {
             if (json[id].comp[index]!=undefined) $(this).css('background-color', json[id].comp[index]==2 ? "#00cc00" : json[id].comp[index]==1 ? "#ffa500" : "#fa8072")
             else $(this).css('background-color', 'transparent');
         });
@@ -89,13 +89,13 @@ $.getJSON("hbdb.json", function(json) {
 
     $('.show').click(function() {
         $('.compat').fadeToggle(200);
-        if ($(this).hasClass('more')) $('.hb_entry #desc').html(json[id].long + ' ')
-        else if ($(this).hasClass('less')) $('.hb_entry #desc').html(json[id].long.substring(0,240) + '... ');
+        if ($(this).hasClass('more')) $('.details .desc').html(json[id].long + ' ')
+        else if ($(this).hasClass('less')) $('.details .desc').html(json[id].long.substring(0,240) + '... ');
         $(this).toggleClass('more less');
     });
 
     $('#btm_x').click(function() {
-        $('.hb_entry').fadeOut(500);
+        $('.details').fadeOut(500);
     });
 
     var options = {
@@ -125,7 +125,7 @@ $.getJSON("hbdb.json", function(json) {
     });
 
     $('#select_l').change(function() {
-        window.location.replace("http://ryuzaki-mrl.github.io/3ds/homebrew/"+this.options[this.options.selectedIndex].value);
+        window.location.replace("http://ryuzaki-mrl.github.io/3ds/homebrew/"+this.options[this.options.selectedIndex].value+"?nolang=&"+document.location.search.substring(1));
     });
 
     $('#select_p').change(function() {
